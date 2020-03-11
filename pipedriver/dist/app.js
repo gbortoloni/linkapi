0f679629dfb27a83e58f8e60af75370bf787effc
Giuliano Bortoloni <giulianoagria@hotmail.com>
2020-03-11 20:19:51 -0300

    fix(pipedrive): fixing queue

--- pipedriver/dist/app.js
+++ pipedriver/dist/app.js
@@ -2,12 +2,14 @@
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 
+var _Queue = require('./lib/Queue'); var _Queue2 = _interopRequireDefault(_Queue);
 var _routes = require('./routes'); var _routes2 = _interopRequireDefault(_routes);
 
 class App {
     constructor() {
         this.server = _express2.default.call(void 0, );
 
+        this.queue();
         this.middlewares();
         this.routes();
     }
@@ -19,6 +21,10 @@ class App {
 
     routes() {
         this.server.use(_routes2.default);
+    }
+
+    queue() {
+        _Queue2.default.processQueue();
     }
 }
 
--- pipedriver/src/app.js
+++ pipedriver/src/app.js
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
+import Queue from './lib/Queue';
 import routes from './routes';
 
 class App {
     constructor() {
         this.server = express();
 
+        this.queue();
         this.middlewares();
         this.routes();
     }
@@ -19,6 +21,10 @@ class App {
 
     routes() {
         this.server.use(routes);
+    }
+
+    queue() {
+        Queue.processQueue();
     }
 }
 
